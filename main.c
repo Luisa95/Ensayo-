45c8015e22a303f687d2f5076cb9643b3ff9bcc5
Luisa95 <luisaflopez33@gmail.com>
2015-08-13 12:59:59 -0500

    main.c

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include "calculadora.h"
+
 int main (void)
   {
-  return(0);
+    int a,b,c;
+    char op;
+ printf("recibir a,b, operacion: A.suma B.Resta C.Multiplicacion D.division");
+ scanf("%d %d %c",&a,&b,&op);
+
+    switch(op)
+    {
+        case 'A':
+            c=suma(a,b);
+            printf("El resultado es %d",c);
+            break;
+
+        case 'B':
+            c=resta(a,b);
+            printf("El resultado es %d",c);
+            break;
+
+        case 'C':
+            c=multiplicacion(a,b);
+            printf("El resultado es %d",c);
+            break;
+
+        case 'D':
+            c=division(a,b);
+            printf("El resultado es %d",c);
+            break;
+    }
+
+return(0);
+
   }
